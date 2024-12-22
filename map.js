25150f3a90064c54dd69c3e0c1d55c33facac0c5
BaadeMeister <baade.julian@web.de>
2024-12-22 13:55:17 +0100

    Update map.js

--- map.js
+++ map.js
@@ -8,21 +8,26 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     maxZoom: 19
 }).addTo(map);
 
-// JSON-Daten laden und Marker hinzufügen
-fetch('locations.json')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(location => {
-            // Popup-Inhalt mit erweiterten Informationen
-            var popupContent = `
-                <div class="popup-content">
-                    <img src="${location.image}" alt="${location.name}">
-                    <strong>${location.name}</strong><br>
-                    Preisbereich: ${location.priceRange}<br>
-                    Spezialisierung: ${location.specialization}
-                </div>
-            `;
-            L.marker(location.coords).addTo(map).bindPopup(popupContent);
-        });
-    })
-    .catch(error => console.error('Fehler beim Laden der Daten:', error));
+// Funktion: Liste erstellen und sortieren
+function populateSidebar(locations) {
+    // Sortiere nach PLZ
+    locations.sort((a, b) => a.zip.localeCompare(b.zip));
+
+    var listContainer = document.getElementById("locations-list");
+    listContainer.innerHTML = ""; // Alte Inhalte löschen
+
+    locations.forEach((location, index) => {
+        var listItem = document.createElement("li");
+        listItem.innerHTML = `
+            <strong>${location.name}</strong><br>
+            Adresse: ${location.address}<br>
+            PLZ: ${location.zip}<br>
+            Preisbereich: ${location.priceRange}<br>
+            Spezialisierung: ${location.specialization}<br>
+            <img src="${location.image}" alt="${location.name}">
+        `;
+        listItem.dataset.index = index;
+
+        // Klick-Event für das Listenelement
+        listItem.addEventListener("click", () => {
+            map.setVi
