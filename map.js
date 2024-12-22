20a7a2eab5fa5f90de1e6c09dc00a184d4f5bb14
BaadeMeister <baade.julian@web.de>
2024-12-22 22:15:59 +0100

    Update map.js

--- map.js
+++ map.js
@@ -12,54 +12,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 let markerArray = [];
 let locationsData = [];
 
-// Funktion zum Anzeigen der Kurzinfo
-function showMarkerInfo(location) {
-    const infoBox = document.getElementById("marker-info");
-    const infoImage = document.getElementById("marker-info-image");
-    const infoName = document.getElementById("marker-info-name");
-    const infoAddress = document.getElementById("marker-info-address");
-    const infoPrice = document.getElementById("marker-info-price");
-
-    // Daten aus dem Marker setzen
-    infoImage.src = location.image;
-    infoName.textContent = location.name;
-    infoAddress.textContent = location.address.split(',')[0];
-    infoPrice.textContent = `Preis: ${location.priceRange}`;
-
-    // Box anzeigen
-    infoBox.classList.remove("hidden");
-}
-
-// Marker aktualisieren
-function updateMarkers(locations) {
-    markerArray.forEach(marker => map.removeLayer(marker));
-    markerArray = [];
-
-    locations.forEach((location, index) => {
-        const marker = L.marker(location.coords).addTo(map);
-        markerArray.push(marker);
-
-        // Event für Marker-Klick
-        marker.on("click", () => {
-            if (window.innerWidth <= 600) {
-                // Mobilansicht: Kurzinfo anzeigen
-                showMarkerInfo(location);
-            } else {
-                // Desktop: Klassisches Popup
-                const popupContent = `
-                    <div>
-                        <img src="${location.image}" alt="${location.name}">
-                        <strong>${location.name}</strong><br>
-                        Preis: ${location.priceRange}
-                    </div>
-                `;
-                marker.bindPopup(popupContent).openPopup();
-            }
-        });
-    });
-}
-
-// Sidebar befüllen
+// Sidebar aktualisieren
 function populateSidebar(locations) {
     const listContainer = document.getElementById("locations-list");
     listContainer.innerHTML = ""; // Alte Inhalte löschen
@@ -79,10 +32,42 @@ function populateSidebar(locations) {
         listItem.addEventListener("click", () => {
             map.setView(location.coords, 14);
             markerArray[index].openPopup();
+            highlightListItem(listItem);
         });
 
         listContainer.appendChild(listItem);
     });
+}
+
+// Marker aktualisieren
+function updateMarkers(locations) {
+    markerArray.forEach(marker => map.removeLayer(marker));
+    markerArray = [];
+
+    locations.forEach((location, index) => {
+        const marker = L.marker(location.coords).addTo(map);
+        markerArray.push(marker);
+
+        marker.on("click", () => {
+            updateMobileInfo(location);
+        });
+    });
+}
+
+// Mobilinfo aktualisieren
+function updateMobileInfo(location) {
+    const mobileInfo = document.getElementById("mobile-info");
+    const mobileInfoContent = document.getElementById("mobile-info-content");
+
+    mobileInfoContent.innerHTML = `
+        <img src="${location.image}" alt="${location.name}">
+        <div>
+            <strong>${location.name}</strong><br>
+            ${location.address.split(',')[0]}<br>
+            Preis: ${location.priceRange}
+        </div>
+    `;
+    mobileInfo.classList.remove("hidden");
 }
 
 // Daten laden
