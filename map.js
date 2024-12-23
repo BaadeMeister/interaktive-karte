68013abe2fd891257ec0a4670fa0289340ac8824
BaadeMeister <baade.julian@web.de>
2024-12-23 10:50:23 +0100

    Update map.js

--- map.js
+++ map.js
@@ -14,14 +14,14 @@ let locationsData = [];
 
 // Marker-Icons
 const defaultIcon = L.icon({
-    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34]
 });
 
 const highlightedIcon = L.icon({
-    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-red.png', // Roter Marker
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png', // Roter Marker
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34]
